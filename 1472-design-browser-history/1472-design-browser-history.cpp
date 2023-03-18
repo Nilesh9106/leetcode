b6fc8bbfc33706f00be739964a43e0a8c62027a5
Nilesh darji <97864081+Nilesh9106@users.noreply.github.com>
2023-03-18 12:16:32 +0530

    Time: 146 ms (61.41%), Space: 57.6 MB (76.70%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -7,15 +7,11 @@ public:
         top++;
     }
     
-    
     void visit(string url) {
         top++;
-        
         if(top < stack.size()){
-            // cout<<"deleting from "<<top<<" to "<<stack.size()-1<<endl;
             stack.erase(stack.begin()+top,stack.begin()+stack.size());
         }
-        // cout<<"inserting "<<url <<" at "<<stack.size()<<" prev "<<stack[stack.size()-1]<<endl;
         stack.push_back(url);
     }
     
@@ -25,18 +21,16 @@ public:
         if(top<0){
             top=0;
         }
-        // cout<<"going back to "<<top<<endl;
-        // cout<<stack[top]<<endl;
         return stack[top];
     }
     
     string forward(int steps) {
-        // cout<<top<<" ";
+        
         top+=steps;
         if(top>stack.size()-1){
             top=stack.size()-1;
         }
-        // cout<<"going forward to "<<top<<endl;
+        
         return stack[top];
     }
 };
